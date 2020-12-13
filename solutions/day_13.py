ab060fff7536116844b61136cf06d848e2fed237
eignatenkov <egor@simplaex.com>
2020-12-13 09:45:00 +0100

    day 13 solved like a boss

--- solutions/day_13.py
+++ solutions/day_13.py
@@ -1,12 +1,15 @@
+import numpy as np
+from sympy import mod_inverse
+
 with open("../data/input_13.txt") as f:
     my_ts = int(f.readline())
     raw_schedule = f.readline().strip().split(',')
 
-print(my_ts)
-print(raw_schedule)
-
-sch_one = [int(x) for x in raw_schedule if x != 'x']
-print(sch_one)
+sch_one = np.array([int(x) for x in raw_schedule if x != 'x'])
 time_to_wait = [x - my_ts % x for x in sch_one]
-print(time_to_wait)
 print(min(time_to_wait)*sch_one[time_to_wait.index(min(time_to_wait))])
+remainders = np.array([x - raw_schedule.index(str(x)) for x in sch_one])
+M = np.prod(sch_one)
+M_i = M // sch_one
+inverses = [mod_inverse(m, s) for m, s in zip(M_i, sch_one)]
+print(np.sum(remainders*M_i*inverses) % M)
