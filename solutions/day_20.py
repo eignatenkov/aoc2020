44d2cd7e7dabf76448fd0c6c7493b42b23fd253e
eignatenkov <egor@simplaex.com>
2020-12-20 10:12:18 +0100

    day 20 solved

--- solutions/day_20.py
+++ solutions/day_20.py
@@ -21,7 +21,6 @@ def read_input(filename):
                     if symb == '#':
                         cur_array[row_index, i] = 1
                 row_index += 1
-        result[cur_name] = cur_array
     return result
 
 
@@ -44,19 +43,37 @@ def align_to_left(border, tile):
     raise Exception(f"tile {tile} didn't match to border{border}")
 
 
+def align_to_above(border, tile):
+    for k in range(4):
+        new_tile = np.rot90(tile, k)
+        if np.array_equal(new_tile[0, :],border):
+            return new_tile
+        new_tile = np.flipud(new_tile)
+        if np.array_equal(new_tile[0, :],border):
+            return new_tile
+    raise Exception(f"tile {tile} didn't match to border{border}")
+
+
 def align_top_left(tiles, match_dict):
     for k, v in match_dict.items():
         if len(v) == 2:
             break
-    print(k, v)
+
+    n_a, n_b = (tiles[i] for i in v)
     for a in range(4):
-        top_left = np.rot90(tiles[k], a)
-        for n in v:
+        rotated = np.rot90(tiles[k], a)
+        for top_left in (rotated, np.flipud(rotated)):
             try:
-                n_t = align_to_left(top_left[:, -1], tiles[n])
+                a_a = align_to_left(top_left[:, -1], n_a)
+                a_b = align_to_above(top_left[-1, :], n_b)
                 return k, top_left
             except:
-                continue
+                try:
+                    a_a = align_to_left(top_left[:, -1], n_b)
+                    a_b = align_to_above(top_left[-1, :], n_a)
+                    return k, top_left
+                except:
+                    continue
 
 
 def make_row(tiles, match_dict, k, top_left):
@@ -74,9 +91,19 @@ def make_row(tiles, match_dict, k, top_left):
                 cur_index = n
                 full_pic = np.hstack((full_pic, cur_right[1:-1, 1:-1]))
                 found = True
+                match_dict[cur_index].remove(n)
                 break
             except:
                 continue
+    # finding the leftmost for the next row
+    next_left = None
+    for n in match_dict[k]:
+        try:
+            next_left = align_to_above(top_left[-1, :], tiles[n])
+            break
+        except:
+            continue
+    return full_pic, match_dict, n, next_left
 
 
 tiles = read_input("../data/input_20.txt")
@@ -93,4 +120,36 @@ for k, v in match_dict.items():
         prod *=k
 print(prod)
 
-print(align_top_left(tiles, match_dict))
+k, top_left = align_top_left(tiles, match_dict)
+curr_pic, match_dict, n, next_left = make_row(tiles, match_dict, k, top_left)
+full_pic = curr_pic
+while True:
+    try:
+        curr_pic, match_dict, n, next_left = make_row(tiles, match_dict, n, next_left)
+        full_pic = np.vstack((full_pic, curr_pic))
+    except:
+        break
+
+
+def check_pattern(pic, i, j):
+    return pic[i, j + 18] and pic[i + 1, j] and pic[i + 1, j + 5] and pic[i + 1, j + 6] and \
+           pic[i + 1, j + 11] and pic[i + 1, j + 12] and pic[i + 1, j + 17] and \
+           pic[i + 1, j + 18] and pic[i + 1, j + 19] and pic[i + 1, j + 17] and \
+           pic[i + 2, j + 1] and pic[i + 2, j + 4] and pic[i + 2, j + 7] and pic[i + 2, j + 10] and \
+           pic[i + 2, j + 13] and pic[i + 2, j + 16]
+
+
+def count_monsters(pic):
+    c = 0
+    for i in range(pic.shape[0] - 2):
+        for j in range(pic.shape[1] - 19):
+            c += check_pattern(pic, i, j)
+    return c
+
+
+for a in range(4):
+    rotated = np.rot90(full_pic, a)
+    for fp in (rotated, np.flipud(rotated)):
+        print(count_monsters(fp))
+
+print(full_pic.sum() - 29*15)
