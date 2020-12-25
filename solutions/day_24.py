80df3e1d6e3bee8ce338e14881a7c3938c2ed007
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2020-12-25 08:35:51 +0100

    day 24 temp

--- solutions/day_24.py
+++ solutions/day_24.py
@@ -1,5 +1,6 @@
 from math import sin, cos, pi
 from collections import Counter
+from tqdm import trange
 
 
 def parse_line(line):
@@ -42,21 +43,67 @@ def walk_route(route):
         x_add, y_add = dir_to_vec(dir)
         x+= x_add
         y += y_add
-    return round(x, 5), round(y, 5)
+    return x, y
 
 
 def count_odd(numbers):
     return sum(n % 2 for n in numbers)
 
 
+def calc_neighbors(x, y):
+    result = []
+    for dir in dir_dict:
+        x_add, y_add = dir_to_vec(dir)
+        result.append((x + x_add, y + y_add))
+    return result
+
+
+def count_black_neighbors(x, y, blacks_rounded):
+    result = 0
+    for n in calc_neighbors(x, y):
+        if (round(n[0], 10), round(n[1], 10)) in blacks_rounded:
+            result += 1
+    return result
+
+
+def run_day(blacks):
+    all_neighbors = set()
+    all_neighbors |= blacks
+    for k in blacks:
+        all_neighbors |= set(calc_neighbors(*k))
+    result = set()
+    result_round = set()
+    blacks_rounded = {(round(n[0], 10), round(n[1], 10)) for n in blacks}
+    for t in all_neighbors:
+        t_round = (round(t[0], 10), round(t[1], 10))
+        n_neighbors = count_black_neighbors(*t, blacks_rounded)
+        if t_round not in blacks_rounded and n_neighbors == 2 or t_round in blacks_rounded and 1 <= n_neighbors <= 2:
+            if t_round not in result_round:
+                result.add(t)
+                result_round.add(t_round)
+    return result
+
+
 if __name__ == "__main__":
     tile_list = []
-    with open("../data/input_24.txt") as f:
+    with open("../data/input_24_test.txt") as f:
         for line in f:
             tile_list.append(parse_line(line.strip()))
 
     tile_count = Counter()
     for route in tile_list:
-        tile_count[walk_route(route)] += 1
+        rp = walk_route(route)
+        tile_count[(round(rp[0], 10), round(rp[1], 10))] += 1
 
     print(count_odd(tile_count.values()))
+
+    blacks = set(k for k, v in tile_count.items() if v % 2)
+    # print(len(blacks))
+    # print(blacks)
+    # blacks = run_day(blacks)
+    # print(len(blacks))
+    for i in range(4):
+        blacks = run_day(blacks)
+        print(i + 1, len(blacks))
+    print(sorted(list(blacks)))
+    print(len(blacks))
